d70eac5a3f017a833bda65c90c95562943cb8839
Loïc <loic.papazian@outlook.fr>
2022-11-23 08:30:20 +0100

    Add doctrine/dbal package and update users

--- database/migrations/2022_11_23_060643_update_and_create_user_role.php
+++ database/migrations/2022_11_23_060643_update_and_create_user_role.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -13,22 +12,22 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('roles',function (Blueprint $table){
-           $table->id()->autoIncrement();
-           $table->char('libelle',50)->comment('name of role');
-           $table->boolean('status')->comment('status of role is active or not active');
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->char('libelle', 50)->comment('name of role');
+            $table->boolean('status')->comment('status of role is active or not active');
         });
-        Schema::create('users',function (Blueprint $table){
+        Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->char('firstname',150)->comment('firstname of user');
-            $table->char('lastname',150)->comment('Last name of user');
-            $table->char('username',255)->comment('Public user name on forum');
+            $table->char('firstname', 150)->comment('firstname of user');
+            $table->char('lastname', 150)->comment('Last name of user');
+            $table->char('username', 255)->comment('Public user name on forum');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-            $table->string('phone',12);
+            $table->string('phone', 12);
             $table->boolean('status')->comment('status of user for dealt account');
             $table->boolean('banish')->comment('if true user is banish of forum');
             $table->unsignedBigInteger('role_id')->comment('ref to role of user');
