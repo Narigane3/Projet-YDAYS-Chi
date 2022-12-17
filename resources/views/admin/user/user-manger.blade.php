cd662aacb4cd54dcfb6bf5bc3bfe799b8386e2c6
Loïc <loic.papazian@outlook.fr>
2022-12-17 21:23:58 +0100

    remove useless meta

--- resources/views/admin/user/user-manger.blade.php
+++ resources/views/admin/user/user-manger.blade.php
@@ -1,10 +1,4 @@
 @extends("layouts.admin")
-{{-- Give more info into head --}}
-@section('head')
-    {{-- Give more info into head --}}
-    {{-- Not index this page form Google --}}
-    <meta name="robot" content="noindex">
-@stop
 
 @section('navbar')
     @parent
