c11c4c75b664a5476fc4a5dc11d832467316d666
Loïc <loic.papazian@outlook.fr>
2023-01-08 16:42:06 +0100

    Fixe lien ver typo

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Chi - @yield('title')</title>
-    <link rel="stylesheet" href="https://use.typekit.net/rzo4cyf.css%22%3E">
+    <link rel="stylesheet" href="https://use.typekit.net/rzo4cyf.css">
     @vite('resources/js/app.js')
     @section('head')
     @show
