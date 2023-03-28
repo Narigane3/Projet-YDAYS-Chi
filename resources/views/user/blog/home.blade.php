14dd98a81a6391bfcd58146cab6aabc210509715
Lord-forgos <johan.rousseau39@gmail.com>
2023-03-28 13:49:14 +0200

    rebuild categories structure

--- resources/views/user/blog/home.blade.php
+++ resources/views/user/blog/home.blade.php
@@ -9,7 +9,7 @@
 @section('content')
     <div class="home-desktop">
         <h1 class="page-title-desktop"><strong>BLOG</strong></h1>
-{{--        <img src="{{ url("/images/blog/home.jpg") }}" alt="Photo de la home page" style="max-width: 100%">--}}
+        <img src="{{ url("/images/blog/home.jpg") }}" alt="Photo de la home page" style="max-width: 100%">
     </div>
     <div class="home-mobile">
         <h1 class="page-title-mobile"><strong>BLOG</strong></h1>
@@ -21,22 +21,19 @@
         <h2 class="categories-title"><strong>Catégories</strong></h2>
 
         <div class="categories">
-            <div class="card-full-img">
-                <img src="{{ url("/images/blog/symptomes_menstruels.jpg") }}" class="card-img" alt="Image carte symptômes menstruels">
+            <div class="card-full-img img-symptomes-menstruels img-size">
                 <h3 class="title-card-full-img">Symptômes menstruels</h3>
-                <span class="badge rounded-pill text-bg-primary">Voir</span>
+                <span class="badge rounded-pill text-bg-primary badge-voir">Voir</span>
             </div>
 
-            <div class="card-full-img">
-                <img src="{{ url("/images/blog/protections.jpg") }}" class="card-img" alt="Image carte symptômes menstruels">
+            <div class="card-full-img img-protection img-size">
                 <h3 class="title-card-full-img">Les protections</h3>
-                <span class="badge rounded-pill text-bg-primary">Voir</span>
+                <span class="badge rounded-pill text-bg-primary badge-voir">Voir</span>
             </div>
 
-            <div class="card-full-img">
-                <img src="{{ url("/images/blog/vivre_avec_ces_regles.jpg") }}" class="card-img" alt="Image carte symptômes menstruels">
+            <div class="card-full-img img-vivre-regles img-size">
                 <h3 class="title-card-full-img">Vivre avec ces règles</h3>
-                <span class="badge rounded-pill text-bg-primary">Voir</span>
+                <span class="badge rounded-pill text-bg-primary badge-voir">Voir</span>
             </div>
         </div>
 
